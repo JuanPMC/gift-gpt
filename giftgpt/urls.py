c0cee9fb275de3e872ea44961343d17b2880fea9
Juan <juan.technova@gmail.com>
2023-09-19 07:34:46 -0500

    Added urls to blog

--- giftgpt/urls.py
+++ giftgpt/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.home),
     path('gift',views.gift),
+    path('three-simple-steps-to-effortless-gift-shopping-with-the-amazon-gift-finder-ai',views.blog_step),
+    path('top-10-reasons-to-embrace-the-amazon-gift-finder-ai',views.blog_top),
 ]
