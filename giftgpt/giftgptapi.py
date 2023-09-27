dacafe6326eb097bd298db8aaa84d13a92cc76e5
Juan <juan.technova@gmail.com>
2023-09-27 09:05:51 -0500

    ADDED REFERAL LINKS

--- giftgpt/giftgptapi.py
+++ giftgpt/giftgptapi.py
@@ -13,18 +13,17 @@ How we met: {}
 How the relationship is now: {}""".format(name,relationship_type,age,intrests,story,state)
 
         self.products = [
-        ("bear.jpg","teddybear","https://www.amazon.com/Gund-Little-Buddy-Bear-Plush/dp/B0074JU5YQ"),
-        ("kittens.jpg","Exploding Kittens Party Pack Card Game","https://www.amazon.com/Exploding-Kittens-Party-Pack-Players/dp/B07CTXHNSL/"),
-        ("cookbook.jpg","Let's Eat: 101 Recipes to Fill Your Heart & Home - A Cookbook","https://www.amazon.com/Lets-Eat-Recipes-Heart-Cookbook/dp/1454946393/"),
-        ("kissChocolate.jpg","Hershey Kisses Milk Chocolate Silver Foil Wrap Candy","https://www.amazon.com/Hershey-Kisses-Chocolate-Approx-60-Tundras/dp/B0BX7H4P1F/"),
-        ("teamug.jpg"," HAPPINESS APPLY HERE Ceramic 15oz Elephant Tea Mug Green","https://www.amazon.com/Volar-Ideas-15oz-Elephant-Green/dp/B071HLGYN9"),
-        ("coloring_book.jpg","ColorIt Modern Art Adult Coloring Book","https://www.amazon.com/ColorIt-Coloring-Drawings-Inspired-Paintings/dp/B09L7L9Z2M"),
-        ("granada.jpg","PenghaiYunfei Collapsible Travel Water Bottle18oz","https://www.amazon.com/Collapsible-Bottle18oz-Reuseable-Carabiner-camouflage/dp/B08P4JKTPV/"),
-        ("popcorn.jpg","MOVIE NIGHT Popcorn Kernels and Popcorn Seasoning","https://www.amazon.com/Urban-Accents-Popcorn-Kernels-Seasoning/dp/B07JN5467R"),
-        ("toolkit.jpg","Mini Tool Set for Dorm, Travel, Office, Home","https://www.amazon.com/ABN-25-Piece-Tri-Fold-Travel-Office/dp/B0B4KQ2QKH"),
-        ("deskorg.jpg","Desk Organizers and Accessories","https://www.amazon.com/EOOUT-Organizers-Accessories-Compartments-Stationery/dp/B0BMDNG7QY")
+        ("bear.jpg","teddybear","https://www.amazon.com/Gund-Little-Buddy-Bear-Plush/dp/B0074JU5YQ?&_encoding=UTF8&tag=technova043-20&linkCode=ur2&linkId=af97b4dba1b3e5fa230e7ab2915fdfdb&camp=1789&creative=9325"),
+        ("kittens.jpg","Exploding Kittens Party Pack Card Game","https://www.amazon.com/Exploding-Kittens-Party-Pack-Players/dp/B07CTXHNSL/?&_encoding=UTF8&tag=technova043-20&linkCode=ur2&linkId=7e22f55a17b1403c843e2ab5ffde3826&camp=1789&creative=9325"),
+        ("cookbook.jpg","Let's Eat: 101 Recipes to Fill Your Heart & Home - A Cookbook","https://www.amazon.com/Lets-Eat-Recipes-Heart-Cookbook/dp/1454946393/?&_encoding=UTF8&tag=technova043-20&camp=1789&creative=9325"),
+        ("kissChocolate.jpg","Hershey Kisses Milk Chocolate Silver Foil Wrap Candy","https://www.amazon.com/Hershey-Kisses-Chocolate-Approx-60-Tundras/dp/B0BX7H4P1F/?&_encoding=UTF8&tag=technova043-20"),
+        ("teamug.jpg"," HAPPINESS APPLY HERE Ceramic 15oz Elephant Tea Mug Green","https://www.amazon.com/Volar-Ideas-15oz-Elephant-Green/dp/B071HLGYN9/?&_encoding=UTF8&tag=technova043-20"),
+        ("coloring_book.jpg","ColorIt Modern Art Adult Coloring Book","https://www.amazon.com/ColorIt-Coloring-Drawings-Inspired-Paintings/dp/B09L7L9Z2M/?&_encoding=UTF8&tag=technova043-20"),
+        ("granada.jpg","PenghaiYunfei Collapsible Travel Water Bottle18oz","https://www.amazon.com/Collapsible-Bottle18oz-Reuseable-Carabiner-camouflage/dp/B08P4JKTPV/?&_encoding=UTF8&tag=technova043-20"),
+        ("popcorn.jpg","MOVIE NIGHT Popcorn Kernels and Popcorn Seasoning","https://www.amazon.com/Urban-Accents-Popcorn-Kernels-Seasoning/dp/B07JN5467R/?&_encoding=UTF8&tag=technova043-20"),
+        ("toolkit.jpg","Mini Tool Set for Dorm, Travel, Office, Home","https://www.amazon.com/ABN-25-Piece-Tri-Fold-Travel-Office/dp/B0B4KQ2QKH/?&_encoding=UTF8&tag=technova043-20"),
+        ("deskorg.jpg","Desk Organizers and Accessories","https://www.amazon.com/EOOUT-Organizers-Accessories-Compartments-Stationery/dp/B0BMDNG7QY/?&_encoding=UTF8&tag=technova043-20")
         ]
-
         self.consultarGPT(promptText)
 
         self.productosTotales =[[]]
