9220ac118d1f15ccaf1409a3a56ecc6b571496e8
Juan <juan.technova@gmail.com>
2023-10-03 09:23:10 -0500

    Added loading screen

--- giftgpt/static/js/global.js
+++ giftgpt/static/js/global.js
@@ -11,48 +11,48 @@
                 format: 'DD/MM/YYYY'
             },
         });
-    
+
         var myCalendar = $('.js-datepicker');
         var isClick = 0;
-    
+
         $(window).on('click',function(){
             isClick = 0;
         });
-    
+
         $(myCalendar).on('apply.daterangepicker',function(ev, picker){
             isClick = 0;
             $(this).val(picker.startDate.format('DD/MM/YYYY'));
-    
+
         });
-    
+
         $('.js-btn-calendar').on('click',function(e){
             e.stopPropagation();
-    
+
             if(isClick === 1) isClick = 0;
             else if(isClick === 0) isClick = 1;
-    
+
             if (isClick === 1) {
                 myCalendar.focus();
             }
         });
-    
+
         $(myCalendar).on('click',function(e){
             e.stopPropagation();
             isClick = 1;
         });
-    
+
         $('.daterangepicker').on('click',function(e){
             e.stopPropagation();
         });
-    
-    
+
+
     } catch(er) {console.log(er);}
     /*[ Select 2 Config ]
         ===========================================================*/
-    
+
     try {
         var selectSimple = $('.js-select-simple');
-    
+
         selectSimple.each(function () {
             var that = $(this);
             var selectBox = that.find('select');
@@ -61,10 +61,38 @@
                 dropdownParent: selectDropdown
             });
         });
-    
+
     } catch (err) {
         console.log(err);
     }
-    
+
 
 })(jQuery);
+
+function changeMultipleCSS() {
+  // Defining all our CSS styles
+  const myStylesLOADING = `
+  display: inline;
+  transform: translateZ(1px);
+  width: 100%;
+  `;
+
+
+  const myStylesHIDE = `
+  background: #000;
+  -webkit-border-radius: 10px;
+  -moz-border-radius: 10px;
+  border-radius: 10px;
+  -webkit-box-shadow: 0px 8px 20px 0px rgba(0, 0, 0, 0.15);
+  -moz-box-shadow: 0px 8px 20px 0px rgba(0, 0, 0, 0.15);
+  box-shadow: 0px 8px 20px 0px rgba(0, 0, 0, 0.15);
+  width: 100%;
+  display: none;
+  `;
+
+  const loading = document.querySelector(".lds-circle");
+  const formulario = document.querySelector(".card-3");
+
+  formulario.style.cssText = myStylesHIDE;
+  loading.style.cssText = myStylesLOADING;
+}
