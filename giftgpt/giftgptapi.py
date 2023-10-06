e2bf172ce2bd25786d79dbc33313d68e3521278b
Juan <juan.technova@gmail.com>
2023-10-06 08:07:36 -0500

    Changed choosing mechanism.

--- giftgpt/giftgptapi.py
+++ giftgpt/giftgptapi.py
@@ -1,6 +1,11 @@
 import openai
 from os import getenv as os_getenv
 
+class Sugerencias(object):
+    def __init__(self,title,description):
+        self.title = title
+        self.description = description
+
 class GiftAI(object):
     """Interfaciong with chatGPT to generate the gift selection"""
 
@@ -12,21 +17,21 @@ Intrests: {}
 How we met: {}
 How the relationship is now: {}""".format(name,relationship_type,age,intrests,story,state)
 
-        self.products = [
-        ("bear.jpg","teddybear","https://www.amazon.com/Gund-Little-Buddy-Bear-Plush/dp/B0074JU5YQ?&_encoding=UTF8&tag=technova043-20&linkCode=ur2&linkId=af97b4dba1b3e5fa230e7ab2915fdfdb&camp=1789&creative=9325"),
-        ("kittens.jpg","Exploding Kittens Party Pack Card Game","https://www.amazon.com/Exploding-Kittens-Party-Pack-Players/dp/B07CTXHNSL/?&_encoding=UTF8&tag=technova043-20&linkCode=ur2&linkId=7e22f55a17b1403c843e2ab5ffde3826&camp=1789&creative=9325"),
-        ("cookbook.jpg","Let's Eat: 101 Recipes to Fill Your Heart & Home - A Cookbook","https://www.amazon.com/Lets-Eat-Recipes-Heart-Cookbook/dp/1454946393/?&_encoding=UTF8&tag=technova043-20&camp=1789&creative=9325"),
-        ("kissChocolate.jpg","Hershey Kisses Milk Chocolate Silver Foil Wrap Candy","https://www.amazon.com/Hershey-Kisses-Chocolate-Approx-60-Tundras/dp/B0BX7H4P1F/?&_encoding=UTF8&tag=technova043-20"),
-        ("teamug.jpg"," HAPPINESS APPLY HERE Ceramic 15oz Elephant Tea Mug Green","https://www.amazon.com/Volar-Ideas-15oz-Elephant-Green/dp/B071HLGYN9/?&_encoding=UTF8&tag=technova043-20"),
-        ("coloring_book.jpg","ColorIt Modern Art Adult Coloring Book","https://www.amazon.com/ColorIt-Coloring-Drawings-Inspired-Paintings/dp/B09L7L9Z2M/?&_encoding=UTF8&tag=technova043-20"),
-        ("granada.jpg","PenghaiYunfei Collapsible Travel Water Bottle18oz","https://www.amazon.com/Collapsible-Bottle18oz-Reuseable-Carabiner-camouflage/dp/B08P4JKTPV/?&_encoding=UTF8&tag=technova043-20"),
-        ("popcorn.jpg","MOVIE NIGHT Popcorn Kernels and Popcorn Seasoning","https://www.amazon.com/Urban-Accents-Popcorn-Kernels-Seasoning/dp/B07JN5467R/?&_encoding=UTF8&tag=technova043-20"),
-        ("toolkit.jpg","Mini Tool Set for Dorm, Travel, Office, Home","https://www.amazon.com/ABN-25-Piece-Tri-Fold-Travel-Office/dp/B0B4KQ2QKH/?&_encoding=UTF8&tag=technova043-20"),
-        ("deskorg.jpg","Desk Organizers and Accessories","https://www.amazon.com/EOOUT-Organizers-Accessories-Compartments-Stationery/dp/B0BMDNG7QY/?&_encoding=UTF8&tag=technova043-20")
-        ]
-        self.consultarGPT(promptText)
+        # self.products = [
+        # ("bear.jpg","teddybear","https://www.amazon.com/Gund-Little-Buddy-Bear-Plush/dp/B0074JU5YQ?&_encoding=UTF8&tag=technova043-20&linkCode=ur2&linkId=af97b4dba1b3e5fa230e7ab2915fdfdb&camp=1789&creative=9325"),
+        # ("kittens.jpg","Exploding Kittens Party Pack Card Game","https://www.amazon.com/Exploding-Kittens-Party-Pack-Players/dp/B07CTXHNSL/?&_encoding=UTF8&tag=technova043-20&linkCode=ur2&linkId=7e22f55a17b1403c843e2ab5ffde3826&camp=1789&creative=9325"),
+        # ("cookbook.jpg","Let's Eat: 101 Recipes to Fill Your Heart & Home - A Cookbook","https://www.amazon.com/Lets-Eat-Recipes-Heart-Cookbook/dp/1454946393/?&_encoding=UTF8&tag=technova043-20&camp=1789&creative=9325"),
+        # ("kissChocolate.jpg","Hershey Kisses Milk Chocolate Silver Foil Wrap Candy","https://www.amazon.com/Hershey-Kisses-Chocolate-Approx-60-Tundras/dp/B0BX7H4P1F/?&_encoding=UTF8&tag=technova043-20"),
+        # ("teamug.jpg"," HAPPINESS APPLY HERE Ceramic 15oz Elephant Tea Mug Green","https://www.amazon.com/Volar-Ideas-15oz-Elephant-Green/dp/B071HLGYN9/?&_encoding=UTF8&tag=technova043-20"),
+        # ("coloring_book.jpg","ColorIt Modern Art Adult Coloring Book","https://www.amazon.com/ColorIt-Coloring-Drawings-Inspired-Paintings/dp/B09L7L9Z2M/?&_encoding=UTF8&tag=technova043-20"),
+        # ("granada.jpg","PenghaiYunfei Collapsible Travel Water Bottle18oz","https://www.amazon.com/Collapsible-Bottle18oz-Reuseable-Carabiner-camouflage/dp/B08P4JKTPV/?&_encoding=UTF8&tag=technova043-20"),
+        # ("popcorn.jpg","MOVIE NIGHT Popcorn Kernels and Popcorn Seasoning","https://www.amazon.com/Urban-Accents-Popcorn-Kernels-Seasoning/dp/B07JN5467R/?&_encoding=UTF8&tag=technova043-20"),
+        # ("toolkit.jpg","Mini Tool Set for Dorm, Travel, Office, Home","https://www.amazon.com/ABN-25-Piece-Tri-Fold-Travel-Office/dp/B0B4KQ2QKH/?&_encoding=UTF8&tag=technova043-20"),
+        # ("deskorg.jpg","Desk Organizers and Accessories","https://www.amazon.com/EOOUT-Organizers-Accessories-Compartments-Stationery/dp/B0BMDNG7QY/?&_encoding=UTF8&tag=technova043-20")
+        # ]
 
-        self.productosTotales =[[]]
+        # self.consultarGPT(promptText)
+        self.consultarCorto(promptText)
 
 
     def apiGPT(self,inputText):
@@ -40,6 +45,36 @@ How the relationship is now: {}""".format(name,relationship_type,age,intrests,st
             ]
         )
         return (response.choices[0].message.content)
+
+
+    def consultarCorto(self,description_text):
+        promptText="""
+Give me a list of 3 alternative presents that whould fit this person:
+
+Information about the person:
+{}
+
+Output format JUST THE ITEMS:
+1. <item 1>:<explanation>
+2. <item 2>:<explanation>
+3. <item 3>:<explanation>
+
+Respod with the leat amount of words
+        """.format(description_text)
+        response = self.apiGPT(promptText)
+        self.arr = []
+        for line in response.splitlines():
+            if line and (line[0] == '1' or line[0] == '2' or line[0] == '3'):
+                sugerencia = Sugerencias(line.split(".")[1].split(":")[0],line.split(".")[1].split(":")[1])
+                self.arr.append(sugerencia)
+
+        # print(promptText)
+        # print("================================")
+        # print(response)
+        # print("================================")
+        # print(self.arr)
+
+        self.url = "https://www.amazon.com/s?k={}&_encoding=UTF8&tag=technova043-20".format(self.arr[0])
 
     def consultarGPT(self,description_text):
 
--- giftgpt/views.py
+++ giftgpt/views.py
@@ -9,11 +9,17 @@ def gift(request):
     if request.method == "GET":
         redirect('/')
     else:
-        respuesta = giftgptapi.GiftAI(request.POST['name'],request.POST['trela'],request.POST['age'],request.POST['intrests'],request.POST['story'],request.POST['status'])
+        # respuesta = giftgptapi.GiftAI(request.POST['name'],request.POST['trela'],request.POST['age'],request.POST['intrests'],request.POST['story'],request.POST['status'])
+        # plantilla = 'gift.html'
 
-        return render(request,'gift.html', {
-            'respuesta': respuesta
+        plantilla = 'giftSeveral.html'
+        respuesta1 = giftgptapi.GiftAI(request.POST['name'],request.POST['trela'],request.POST['age'],request.POST['intrests'],request.POST['story'],request.POST['status'])
+        respuesta = respuesta1.arr
+
+        return render(request,plantilla, {
+            'gifts': respuesta
         })
+
 def blog_top(request):
     return render(request,"blog-top10.html")
 def blog_step(request):
